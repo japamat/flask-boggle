5928bb873f152f111132429716ab7d735e26d52a
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-04-12 17:14:06 -0700

    all the work we did in class

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, redirect, session, render_template
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, redirect, session, render_template, jsonify
 from boggle import Boggle
 
 boggle_game = Boggle()
@@ -7,13 +6,35 @@ boggle_game = Boggle()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "oh-so-secret"
-debug = DebugToolbarExtension(app)
 
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 @app.route('/')
 def root():
+    global boggle_game
+    boggle_game = Boggle()
     session["board"] = boggle_game.make_board()
     board = session["board"]
-    return render_template('board.html',
-                            board=board)
+    return render_template('board.html', board=board)
+
+
+@app.route('/answer', methods=["POST"])
+def answer_handling():
+    data = request.form['answer']
+    result = boggle_game.check_valid_word(board=session["board"] , word=data)
+    server_response = jsonify({'result': result, 'word': data})
+    return server_response
+
+
+@app.route('/new_game', methods=["POST"])
+def user_handling():
+    final = request.values
+    if session.get('user'):
+        temp = session['user'].games
+        temp += 1
+        session['user'].games = temp
+        if session['user'].high < final.high:
+            session['user'].high = final.high
+    else:
+        session['user'] = {'high': final.high, 'games': 1}
+    session['user'] = session['user']
+    return redirect('/')
--- static/main.js
+++ static/main.js
@@ -1,10 +1,46 @@
 $(function(){
-    
-    
+    var TIMER = $('#timer')
+
+    let decreaseTime = (setInterval(function() {
+        let curTime = Number(TIMER.text())
+        curTime--
+        if (curTime <= 0){
+            clearInterval(decreaseTime)
+            $('#game').remove()
+            $('.container').append('<button id="newGame" class="btn btn-primary btn-block">New Game</button>')
+            $('#newGame').on('click', newGame)
+            // $('#submit-guess-btn').click(false)
+        }
+        TIMER.text(curTime)
+    }, 1000))
+
     $('#submit-guess-btn').on('click', function (evt) {
         evt.preventDefault();
+        let guess = $('#user-guess').val()
+        $('form').trigger('reset')
+        let answerObj = {answer: guess}
+        let answerSubmission =  $.ajax({
+            type: 'POST',
+            url:'/answer',
+            data: answerObj,
+            success: updateScore
+        })
         
     })
-
+    function updateScore (response){
+        let currentScore = $('#score').text()
+        if (response.result === 'ok') {
+            $('#score').text((Number(currentScore)+response.word.length))
+        }
+    }
+    function newGame(){
+       let score = Number($('#score').text())
+       let final = {high: score}
+       let finalObj =  $.ajax({
+            type: 'POST',
+            url:'/new_game',
+            data: final,
+        })
+    }
 
 })
